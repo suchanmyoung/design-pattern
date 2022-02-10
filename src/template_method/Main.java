4a6c6788f0c4681c97bfde4d40c3a7b2b979b20b
suchanmyung <aud248@naver.com>
2022-02-10 16:33:14 +0900

    feat: Template Method

---
+++ src/template_method/Main.java
@@ -0,0 +1,18 @@
+package template_method;
+
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AbstractDisplay d1 = new CharDisplay('H');
+        AbstractDisplay d2 = new StringDisplay("Hello, World");
+        AbstractDisplay d3 = new StringDisplay("안녕하세요.");
+
+        d1.display();
+        d2.display();
+        d3.display();
+
+
+    }
+}
